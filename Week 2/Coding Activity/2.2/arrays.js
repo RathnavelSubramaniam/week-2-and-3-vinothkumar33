647215278e9207e52a83d0d55651e4d6006e4227
vinoth kumar <91111832+vinothkumar33@users.noreply.github.com>
2021-10-20 13:18:04 +0530

    Add files via upload

---
+++ Week 2/Coding Activity/2.2/arrays.js
@@ -0,0 +1,9 @@
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let elementAtIndexFive=(numArray[5]);
+
+// TODO: declare the variable elementAtIndexFive and assign it the v
+
+
+// This will print the value of variable elementAtIndexFive to the c
+
+console.log('The array element with index 5 is:', elementAtIndexFive);
